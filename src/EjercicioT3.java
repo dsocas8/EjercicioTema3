25cc02b9bfd3445f77bbbd54b750f787610a0354
Diego <d.socas@udc.es>
2022-11-13 12:54:05 +0100

    termina ejercicio

--- src/EjercicioT3.java
+++ src/EjercicioT3.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class EjercicioT3 {
+public class EjercicioT3 {
+
+    public static void main(String[] args) {
+
+        int res = suma(10, 20, 30);
+        System.out.println(res);
+
+        Coche miCoche = new Coche();
+        miCoche.addPuerta();
+        System.out.println(miCoche.numPuertas);
+
+    }
+
+    public static int suma (int a, int b, int c){
+        return (a+b+c);
+    }
+
+}
+
+class Coche {
+
+    public int numPuertas = 0;
+
+    public void addPuerta(){
+        numPuertas++;
+    }
+
 }
